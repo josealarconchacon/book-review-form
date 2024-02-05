36fd15db5c9e3eb67db919521c25fd488ebb33ae
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-02-05 12:58:57 -0500

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <div style="text-align: center">
-      <app-books></app-books>
+    <nav class="navbar navbar-expand navbar-light bg-light">
+      <a class="navbar-brand" style="margin: 15px;">{{ siteTitle }}</a>
+      <ul class="nav nav-pills">
+        <li><a class="nav-link" routerLink="/home">Book</a></li>
+        <li><a class="nav-link" routerLink="/books">Book Form</a></li>
+      </ul>
+    </nav>
+    <div class="container">
+      <router-outlet></router-outlet>
     </div>
   `,
 })
