9822c5b5ff9214ad5d54f43e7dd009740e2ed392
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-02-04 21:03:17 -0500

    httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -18,7 +19,7 @@ import { ReviewComponent } from './shared/review.component';
     SetToSpacesPipe,
     ReviewComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
