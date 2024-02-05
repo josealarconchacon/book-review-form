c585f65be2437647a01ad34b95097187a8f8ea83
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-02-05 14:03:31 -0500

    pass data to detail component

--- src/app/books/book-detail.component.ts
+++ src/app/books/book-detail.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Book } from './book';
 
 @Component({
   // selector: 'app-book-detail',
   templateUrl: './book-detail.component.html',
   styleUrls: ['./book-detail.component.css'],
 })
-export class BookDetailComponent {
+export class BookDetailComponent implements OnInit {
   pageTitle: string = 'Book Detail';
+  books: Book | undefined;
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+  }
+
+  onBack(): void {
+    this.router.navigate(['/books']);
+  }
 }
